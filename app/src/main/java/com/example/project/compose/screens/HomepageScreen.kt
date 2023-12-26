d11487a6930168f3fd7db5a2e46765a416596974
Pavel Zhukovskiy <pavel20122004@mail.ru>
2023-12-27 00:05:32 +0300

    added swiper for images, index, refactor code

--- app/src/main/java/com/example/project/compose/screens/HomepageScreen.kt
+++ app/src/main/java/com/example/project/compose/screens/HomepageScreen.kt
@@ -1,12 +1,21 @@
 package com.example.project.compose.screens
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.PageSize
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
@@ -14,6 +23,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -23,11 +33,22 @@ import com.example.project.compose.widgets.AnimatedHeaderWidget
 
 //scrollState.maxValue == 1423
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun HomepageScreen() {
+
     val scrollState = rememberScrollState()
+    val images = listOf(
+        R.drawable.king_kohli,
+        R.drawable.football,
+        R.drawable.football2,
+    )
 
     Box {
+
+        val pagerState = rememberPagerState()
+
+
         Column(
             modifier = Modifier
                 .fillMaxSize()
@@ -37,14 +58,65 @@ fun HomepageScreen() {
                 modifier = Modifier
                     .fillMaxWidth()
                     .height(550.dp),
-                contentAlignment = Alignment.Center
+                contentAlignment = Alignment.Center,
             ) {
-                Image(
-                    painter = painterResource(id = R.drawable.king_kohli),
-                    contentDescription = null,
-                    modifier = Modifier.fillMaxSize(),
-                    contentScale = ContentScale.Crop
-                )
+                HorizontalPager(
+                    pageCount = images.size,
+                    state = pagerState,
+                    key = {images[it]},
+                    pageSize = PageSize.Fill
+                ) { index ->
+                    Image(
+                        painter = painterResource(id = images[index]),
+                        contentDescription = null,
+                        modifier = Modifier.fillMaxSize(),
+                        contentScale = ContentScale.Crop
+                    )
+                    Column(
+                        modifier = Modifier
+                            .padding(top = 350.dp)
+                    ) {
+                        Box(
+                            modifier = Modifier
+                                .height(40.dp)
+                                .fillMaxWidth()
+                                .padding(horizontal = 120.dp)
+                                .background(Color.White)
+                        ) {
+                            Row(
+                                modifier = Modifier
+                                    .height(40.dp)
+                                    .fillMaxWidth(),
+                                verticalAlignment = Alignment.CenterVertically,
+                                horizontalArrangement = Arrangement.SpaceAround
+                            ) {
+                                repeat(images.size) {
+                                    Box(
+                                        contentAlignment = Alignment.Center,
+                                        modifier = Modifier
+                                            .height(30.dp)
+                                            .width(30.dp)
+                                            .background(if (it == index) Color.Black else Color.LightGray)
+                                    ) {
+                                        Text(text = it.toString(), color = (if (it == index) Color.White else Color.Black))
+                                    }
+                                }
+                            }
+                        }
+
+                        //последний box с текстом
+                        if (index == images.size - 1) {
+                            Text(
+                                text = stringResource(id = R.string.check_all_news),
+                                color = Color.White,
+                                modifier = Modifier
+                                    .padding(top = 10.dp)
+                                    .fillMaxWidth()
+                                    .wrapContentSize(align = Alignment.Center)
+                            )
+                        }
+                    }
+                }
             }
 
             Text(
